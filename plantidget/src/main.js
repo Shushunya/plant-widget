458132b095518aa2cf781782b7d9f7507d613162
Shushunya <mremarka314@gmail.com>
2026-07-01 11:47:01 +0100

    add prevMonth & nextMonth btns

--- plantidget/src/main.js
+++ plantidget/src/main.js
@@ -1,37 +1,62 @@
-// const { invoke } = window.__TAURI__.core;
-
 const { getCurrentWindow } = window.__TAURI__.window;
 const appWindow = getCurrentWindow();
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+// ELEMENTS
 const monthYearElement = document.getElementById("monthYear");
+// BUTTONS
 const closeWidgetBtn = document.getElementById("closeBtn");
+const prevMonthBtn = document.getElementById("prevMonth");
+const nextMonthBtn = document.getElementById("nextMonth");
 
-function getCurrentMonthYear() {
-  const now = new Date();
-  const year = now.getFullYear();
-  let month = months[now.getMonth()];
+// STATE VARS
+let currMonth;
+let currYear;
 
+function updateMonthDisplay() {
   if (monthYearElement) {
-    monthYearElement.textContent = `${month} ${year}`;
-    console.log("Success")
+    monthYearElement.textContent = `${months[currMonth]} ${currYear}`;
   } else {
     console.error("Fail: Could not find an element with the ID 'monthYear'.");
   }
-
 }
 
 
 window.addEventListener("DOMContentLoaded", () => {
+  const now = new Date();
+  currMonth = now.getMonth();
+  currYear = now.getFullYear();
+  updateMonthDisplay();
 
-  getCurrentMonthYear();
+  if (nextMonthBtn) {
+    nextMonthBtn.addEventListener("click", () => {
+      if (currMonth === 11) {
+        currMonth = 0;
+        currYear += 1;
+      } else {
+        currMonth += 1;
+      }
+      updateMonthDisplay();
+    });
+  }
 
-  const closeBtn = document.getElementById("closeBtn");
+  if (prevMonthBtn) {
+    prevMonthBtn.addEventListener("click", () => {
+      if (currMonth === 0) {
+        currMonth = 11;
+        currYear -= 1;
+      } else {
+        currMonth -= 1;
+      }
+      updateMonthDisplay();
+    });
+  }
+  
+
   if (closeWidgetBtn) {
     closeWidgetBtn.addEventListener("click", () => {
       appWindow.close();
     });
   }
-
 });
