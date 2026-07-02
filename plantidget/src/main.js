0e4c793c35b8607e6cd122a6a8f8740b3e585401
Shushunya <mremarka314@gmail.com>
2026-07-02 10:50:26 +0100

    add calendar grid

--- plantidget/src/main.js
+++ plantidget/src/main.js
@@ -2,9 +2,11 @@ const { getCurrentWindow } = window.__TAURI__.window;
 const appWindow = getCurrentWindow();
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 // ELEMENTS
 const monthYearElement = document.getElementById("monthYear");
+const calendarGrid = document.getElementById("calendarDays");
 // BUTTONS
 const closeWidgetBtn = document.getElementById("closeBtn");
 const prevMonthBtn = document.getElementById("prevMonth");
@@ -20,8 +22,35 @@ function updateMonthDisplay() {
   } else {
     console.error("Fail: Could not find an element with the ID 'monthYear'.");
   }
+
+  renderCalendarGrid();
 }
 
+function renderCalendarGrid() {
+  // Clear out the old calendar days before generating new ones
+  if (!calendarGrid) return;
+  calendarGrid.innerHTML = "";
+
+  const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+  console.log(`First day of ${months[currMonth]}-${currYear} is ${weekDays[firstDayOfMonth]}`)
+  
+  const daysInMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  console.log(daysInMonth)
+
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("empty-day");
+    calendarGrid.appendChild(newDiv);
+    console.log("Empty day")
+  }
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("calendar-day");
+    newDiv.innerText = `${i}`;
+    calendarGrid.appendChild(newDiv);
+  }
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   const now = new Date();
