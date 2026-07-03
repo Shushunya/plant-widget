17677428892133c3e001bb27c49d92b503a8d44e
Shushunya <mremarka314@gmail.com>
2026-07-03 13:35:38 +0100

    add main window init

--- plantidget/src/widget.js
+++ plantidget/src/widget.js
@@ -1,4 +1,4 @@
-const { getCurrentWindow } = window.__TAURI__.window;
+const { getCurrentWindow, Window } = window.__TAURI__.window;
 const appWindow = getCurrentWindow();
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -7,15 +7,21 @@ const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 // ELEMENTS
 const monthYearElement = document.getElementById("monthYear");
 const calendarGrid = document.getElementById("calendarDays");
-const calendarFragment = new DocumentFragment();
 // BUTTONS
 const closeWidgetBtn = document.getElementById("closeBtn");
-const prevMonthBtn = document.getElementById("prevMonth");
-const nextMonthBtn = document.getElementById("nextMonth");
+const prevMonthBtn = document.getElementById("prevMonthBtn");
+const nextMonthBtn = document.getElementById("nextMonthBtn");
+
+const plantListBtn = document.getElementById("openPlantListBtn");
+const openMainBtn = document.getElementById("openMainBtn");
+const toggleThemeBtn = document.getElementById("themeToggleBtn");
+const settingsBtn = document.getElementById("openSettingsBtn");
 
 // STATE VARS
 let currMonth;
 let currYear;
+
+console.log(appWindow)
 
 function updateMonthDisplay() {
   if (monthYearElement) {
@@ -35,37 +41,37 @@ function renderCalendarGrid() {
   const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(); // starting with 0 for Sunday
   const daysInMonth = new Date(currYear, currMonth + 1, 0).getDate();
 
+  const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < firstDayOfMonth - 1; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("empty-day");
-    calendarFragment.append(newDiv);
+    fragment.appendChild(newDiv);
   }
 
   for (let i = 1; i <= daysInMonth; i++) {
     const newDiv = document.createElement("div");
     const dateString = `${currYear}-${currMonth}-${i}`;
     newDiv.classList.add("calendar-day");
+    if (i == 2) {
+      newDiv.classList.add("fertilize-day");
+    } else if (i == 5) {
+      newDiv.classList.add("water-day");
+    };
+    
     newDiv.setAttribute("data-date", dateString);
     newDiv.innerText = `${i}`;
-    calendarFragment.append(newDiv);
+    fragment.appendChild(newDiv);
   }
-  calendarGrid.append(calendarFragment);
+  calendarGrid.appendChild(fragment);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+
   const now = new Date();
   currMonth = now.getMonth();
   currYear = now.getFullYear();
   updateMonthDisplay();
-  // Get the theme button
-  const themeToggleBtn = document.getElementById("themeToggleBtn");
-
-  if (themeToggleBtn) {
-    themeToggleBtn.addEventListener("click", () => {
-      // .toggle() adds the class if it's missing, and removes it if it's there!
-      document.body.classList.toggle("dark-theme");
-    });
-  }
 
   if (nextMonthBtn) {
     nextMonthBtn.addEventListener("click", () => {
@@ -90,7 +96,35 @@ window.addEventListener("DOMContentLoaded", () => {
       updateMonthDisplay();
     });
   }
-  
+
+  if (themeToggleBtn) {
+    themeToggleBtn.addEventListener("click", () => {
+      // .toggle() adds the class if it's missing, and removes it if it's there!
+      document.body.classList.toggle("dark-theme");
+    });
+  }
+
+  if (openMainBtn) {
+    openMainBtn.addEventListener("click", async () => {
+      // const mainWindow = await Window.getByLabel("main");
+      // await mainWindow.show();
+      // await mainWindow.setFocus();
+      console.log("Clicked 'Open Dashboard' Button.")
+    });
+  }
+
+  if (plantListBtn) {
+    plantListBtn.addEventListener("click", async () => {
+      console.log("Clicked on 'Show palnts list' btn.")
+    })
+  }
+
+  if (settingsBtn) {
+    settingsBtn.addEventListener("click", async () => {
+      console.log("Settings");
+    })
+  }
+
 
   if (closeWidgetBtn) {
     closeWidgetBtn.addEventListener("click", () => {
